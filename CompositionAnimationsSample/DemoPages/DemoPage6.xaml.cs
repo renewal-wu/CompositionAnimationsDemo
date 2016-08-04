2e91045ea6cf6c063073caa05390083666fd65a1
renewal-wu <rc3176@hotmail.com>
2016-08-04 11:22:57 +0800

    Add InteractionTracker demo.

--- CompositionAnimationsSample/DemoPages/DemoPage6.xaml.cs
+++ CompositionAnimationsSample/DemoPages/DemoPage6.xaml.cs
@@ -2,15 +2,19 @@
 using System.Numerics;
 using System.Threading.Tasks;
 using Windows.UI.Composition;
+using Windows.UI.Composition.Interactions;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
 
 namespace CompositionAnimationsSample.DemoPages
 {
     public sealed partial class DemoPage6 : Page
     {
+        private Visual rootVisual;
         private Compositor compositor;
-        private CompositionPropertySet propertySet;
+        private InteractionTracker tracker;
+        private VisualInteractionSource interactionSource;
 
         public DemoPage6()
         {
@@ -18,18 +22,19 @@ namespace CompositionAnimationsSample.DemoPages
 
             SurfaceLoader.Initialize(ElementCompositionPreview.GetElementVisual(this).Compositor);
 
-            compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+            rootVisual = ElementCompositionPreview.GetElementVisual(this);
+            compositor = rootVisual.Compositor;
 
-            StartExpressionAnimation();
+            AddInteractableImage();
         }
 
-        private async void StartExpressionAnimation()
+        private async void AddInteractableImage()
         {
-            // 將 containerVisual 與 spriteVisual 新增至 TargetGrid 中
+            // 將 containerVisual 與 spriteVisual 新增至 DemoPage6 中
             var containerVisual = compositor.CreateContainerVisual();
             var spriteVisual = compositor.CreateSpriteVisual();
             containerVisual.Children.InsertAtTop(spriteVisual);
-            ElementCompositionPreview.SetElementChildVisual(TargetGrid, containerVisual);
+            ElementCompositionPreview.SetElementChildVisual(this, containerVisual);
 
             // 讀圖
             var targetBrush = await GenerateCompositionBrush();
@@ -37,29 +42,47 @@ namespace CompositionAnimationsSample.DemoPages
             // 將圖片讀進 spriteVisual
             spriteVisual.Brush = targetBrush;
             spriteVisual.Size = new Vector2(300f, 300f);
-            spriteVisual.CenterPoint = new Vector3(150f, 150f, 0f);
 
-            // 產生 PropertySet，並定義 Option1、Option2
-            propertySet = compositor.CreatePropertySet();
-            propertySet.InsertScalar("Option1", 0.0f);
-            propertySet.InsertScalar("Option2", 0.0f);
+            // 建立 InteractionTracker
+            tracker = InteractionTracker.Create(compositor);
 
-            //// 取得 propertySet 內的屬性
-            // float currentOption1;
-            // propertySet.TryGetScalar("Option1", out currentOption1);
+            // 設定位移位置的最大值與最小值
+            // 類似 ScrollViewer.ScrollableHeight, ScrollViewer.ScrollableWidth
+            tracker.MaxPosition = new Vector3(0, 300f, 0);
+            tracker.MinPosition = new Vector3();
 
-            // Opacity 會根據 Option1 呈現線性動畫
-            var expressionAnimation = compositor.CreateExpressionAnimation();
-            expressionAnimation.SetReferenceParameter("PropertySet", propertySet);
-            expressionAnimation.Expression = "PropertySet.Option1";
-            spriteVisual.StartAnimation("Opacity", expressionAnimation);
+            // 建立互動來源 (必須指定互動範圍區域)
+            interactionSource = VisualInteractionSource.Create(rootVisual);
 
-            // Scale.X 與 Scale.Y 會根據 Option2 呈現平方根動畫
-            expressionAnimation = compositor.CreateExpressionAnimation();
-            expressionAnimation.SetReferenceParameter("PropertySet", propertySet);
-            expressionAnimation.Expression = "Square(PropertySet.Option2)";
-            spriteVisual.StartAnimation("Scale.X", expressionAnimation);
-            spriteVisual.StartAnimation("Scale.Y", expressionAnimation);
+            // 設定互動方式
+            interactionSource.PositionYSourceMode = InteractionSourceMode.EnabledWithInertia;
+
+            // 將互動來源加至 tracker
+            tracker.InteractionSources.Add(interactionSource);
+
+            // 用 tracker 來設定 animation
+            const int topMargin = 100;
+            var offsetYAnimation = compositor.CreateExpressionAnimation("-tracker.Position.Y + topMargin");
+            offsetYAnimation.SetReferenceParameter("tracker", tracker);
+            offsetYAnimation.SetScalarParameter("topMargin", topMargin);
+            spriteVisual.StartAnimation("Offset.Y", offsetYAnimation);
+
+            //=======================================================
+            // 讓 interactionSource 支援 Scale
+
+            //tracker.MaxScale = 3.0f;
+            //tracker.MinScale = 0.5f;
+
+            //interactionSource.ScaleSourceMode = InteractionSourceMode.EnabledWithoutInertia;
+
+            //var scaleAnimation = compositor.CreateExpressionAnimation("tracker.Scale");
+            //scaleAnimation.SetReferenceParameter("tracker", tracker);
+            //spriteVisual.StartAnimation("Scale.X", scaleAnimation);
+            //spriteVisual.StartAnimation("Scale.Y", scaleAnimation);
+
+            //var maxYAnimation = compositor.CreateExpressionAnimation("spriteVisual.Size.Y * spriteVisual.Scale.Y");
+            //maxYAnimation.SetReferenceParameter("spriteVisual", spriteVisual);
+            //tracker.StartAnimation("MaxPosition.Y", maxYAnimation);
         }
 
         private async Task<CompositionBrush> GenerateCompositionBrush()
@@ -68,27 +91,12 @@ namespace CompositionAnimationsSample.DemoPages
             return compositor.CreateSurfaceBrush(surface);
         }
 
-        private void SetOption(float optionX, float optionY)
+        private void Pointer_Pressed(object sender, PointerRoutedEventArgs e)
         {
-            var scalarAnimation = compositor.CreateScalarKeyFrameAnimation();
-            scalarAnimation.Duration = TimeSpan.FromMilliseconds(800);
-            scalarAnimation.InsertKeyFrame(1f, optionX);
-            propertySet.StartAnimation("Option1", scalarAnimation);
-
-            scalarAnimation = compositor.CreateScalarKeyFrameAnimation();
-            scalarAnimation.Duration = TimeSpan.FromMilliseconds(800);
-            scalarAnimation.InsertKeyFrame(1f, optionY);
-            propertySet.StartAnimation("Option2", scalarAnimation);
-        }
-
-        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            SetOption(1f, 1f);
-        }
-
-        private void Button_Click_1(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            SetOption(0f, 0f);
+            if (e.Pointer.PointerDeviceType == Windows.Devices.Input.PointerDeviceType.Touch)
+            {
+                interactionSource.TryRedirectForManipulation(e.GetCurrentPoint(RootContainer));
+            }
         }
     }
 }
